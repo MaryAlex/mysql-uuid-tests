c2520aaffdba609a40a3e292e04262d01c02857f
MaryAlex <worker96@mail.ru>
2018-07-24 10:45:20 +0300

    Create script fix

--- create.py
+++ create.py
@@ -11,10 +11,10 @@ print(delta_v1_bin.microseconds)
 before_v4_bin = datetime.datetime.now()
 requests.get("http://localhost:12101/create")
 delta_v4_bin = datetime.datetime.now() - before_v4_bin
-print(delta_v1_bin.microseconds)
+print(delta_v4_bin.microseconds)
 
 # UUID forth version as varchar
 before_v4_char = datetime.datetime.now()
 requests.get("http://localhost:12100/create")
 delta_v4_char = datetime.datetime.now() - before_v4_char
-print(delta_v1_bin.microseconds)
+print(delta_v4_char.microseconds)
